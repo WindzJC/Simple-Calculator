7e802744094b2c56f108e77488dafa2599fe11c5
WindzJC <windellejcr@gmail.com>
2022-11-23 01:42:15 +0800

    Simple Calcu

--- app/src/main/java/com/example/calculator/MainActivity.java
+++ app/src/main/java/com/example/calculator/MainActivity.java
@@ -6,67 +6,150 @@ import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.example.calculator.R;
-
 public class MainActivity extends AppCompatActivity {
+    EditText et1,et2,et3;
+    String resultMessage;
+    double n1,n2,n3,result;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        }
 
-    }
-    public void Add(View v){
-        EditText et1 =(EditText)findViewById(R.id.editTextNumber1);
-        EditText et2 =(EditText)findViewById(R.id.editTextNumber2);
-        EditText et4 =(EditText)findViewById(R.id.editTextNumber4);
-        EditText et3 =(EditText)findViewById(R.id.editTextNumber3);
+            public void Add(View v){
+                 et1 =(EditText)findViewById(R.id.editTextNumber1);
+                 et2 =(EditText)findViewById(R.id.editTextNumber2);
+                 et3 =(EditText)findViewById(R.id.editTextNumber3);
 
-        int n1 = Integer.parseInt(et1.getText().toString());
-        int n2 = Integer.parseInt(et2.getText().toString());
-        int n4 = Integer.parseInt(et4.getText().toString());
-        int result = n1+n2+n4;
 
-        et3.setText("Total Value" +result);
-    }
+                if(et1.getText().toString().isEmpty() || et2.getText().toString().isEmpty()|| et3.getText().toString().isEmpty()){
+                    n1 = 0;
+                    n2 = 0;
+                    n3 = 0;
+                }else {
+                    n1 = Double.parseDouble(et1.getText().toString());
+                    n2 = Double.parseDouble(et2.getText().toString());
+                    n3 = Double.parseDouble(et3.getText().toString());
+                }
+                result = n1+n2+n3;
+
+                    resultMessage = result + " is the answer";
+
+                    // Create Bundle instance, this will allow transfer of data from Activity to DialogFragment
+                    Bundle args = new Bundle();
+                    args.putString("result", resultMessage);
+
+                    // Create a dialog instance
+                    Fragment dialogFragmentImp = new Fragment();
+                    // Pass on dialog argument(args), the result
+                    dialogFragmentImp.setArguments(args);
+                    // Display the Dialog
+                    dialogFragmentImp.show(getSupportFragmentManager(), "Display Result");
+                    // Reset EditTexts
+                    clearEditText();
+                }
+
 
     public void Subtract(View v){
-        EditText et1 =(EditText)findViewById(R.id.editTextNumber1);
-        EditText et2 =(EditText)findViewById(R.id.editTextNumber2);
-        EditText et4 =(EditText)findViewById(R.id.editTextNumber4);
-        EditText et3 =(EditText)findViewById(R.id.editTextNumber3);
+                 et1 =(EditText)findViewById(R.id.editTextNumber1);
+                 et2 =(EditText)findViewById(R.id.editTextNumber2);
+                 et3 =(EditText)findViewById(R.id.editTextNumber3);
+        if(et1.getText().toString().isEmpty() || et2.getText().toString().isEmpty()|| et3.getText().toString().isEmpty()){
+            n1 = 0;
+            n2 = 0;
+            n3 = 0;
+        }else {
+            n1 = Double.parseDouble(et1.getText().toString());
+            n2 = Double.parseDouble(et2.getText().toString());
+            n3 = Double.parseDouble(et3.getText().toString());
+        }
+        result = n1-n2-n3;
 
-        int n1 = Integer.parseInt(et1.getText().toString());
-        int n2 = Integer.parseInt(et2.getText().toString());
-        int n4 = Integer.parseInt(et4.getText().toString());
-        int result = n1-n2-n4;
 
-        et3.setText("Subtract Value" +result);
+        resultMessage = result + " is the answer";
+
+        // Create Bundle instance, this will allow transfer of data from Activity to DialogFragment
+        Bundle args = new Bundle();
+        args.putString("result", resultMessage);
+
+        // Create a dialog instance
+        Fragment dialogFragmentImp = new Fragment();
+        // Pass on dialog argument(args), the result
+        dialogFragmentImp.setArguments(args);
+        // Display the Dialog
+        dialogFragmentImp.show(getSupportFragmentManager(),"Display Result");
+        // Reset EditTexts
+        clearEditText();
+
+
+            }
+            public void Multiply(View v){
+                 et1 =(EditText)findViewById(R.id.editTextNumber1);
+                 et2 =(EditText)findViewById(R.id.editTextNumber2);
+                 et3 =(EditText)findViewById(R.id.editTextNumber3);
+                if(et1.getText().toString().isEmpty() || et2.getText().toString().isEmpty()|| et3.getText().toString().isEmpty()){
+                    n1 = 0;
+                    n2 = 0;
+                    n3 = 0;
+                }else {
+                    n1 = Double.parseDouble(et1.getText().toString());
+                    n2 = Double.parseDouble(et2.getText().toString());
+                    n3 = Double.parseDouble(et3.getText().toString());
+                }
+                result = n1*n2*n3;
+
+                resultMessage = result + " is the answer";
+
+                // Create Bundle instance, this will allow transfer of data from Activity to DialogFragment
+                Bundle args = new Bundle();
+                args.putString("result", resultMessage);
+
+                // Create a dialog instance
+                Fragment dialogFragmentImp = new Fragment();
+                // Pass on dialog argument(args), the result
+                dialogFragmentImp.setArguments(args);
+                // Display the Dialog
+                dialogFragmentImp.show(getSupportFragmentManager(),"Display Result");
+                // Reset EditTexts
+                clearEditText();
+
+            }
+            public void Divide(View v){
+                 et1 =(EditText)findViewById(R.id.editTextNumber1);
+                 et2 =(EditText)findViewById(R.id.editTextNumber2);
+                 et3 =(EditText)findViewById(R.id.editTextNumber3);
+
+                if(et1.getText().toString().isEmpty() || et2.getText().toString().isEmpty()|| et3.getText().toString().isEmpty()){
+                    n1 = 0;
+                    n2 = 0;
+                    n3 = 0;
+                }else {
+                    n1 = Double.parseDouble(et1.getText().toString());
+                    n2 = Double.parseDouble(et2.getText().toString());
+                    n3 = Double.parseDouble(et3.getText().toString());
+                }
+                result = n1/n2/n3;
+
+                resultMessage = result + " is the answer";
+
+                // Create Bundle instance, this will allow transfer of data from Activity to DialogFragment
+                Bundle args = new Bundle();
+                args.putString("result", resultMessage);
+
+                // Create a dialog instance
+                Fragment dialogFragmentImp = new Fragment();
+                // Pass on dialog argument(args), the result
+                dialogFragmentImp.setArguments(args);
+                // Display the Dialog
+                dialogFragmentImp.show(getSupportFragmentManager(),"Display Result");
+                // Reset EditTexts
+                clearEditText();
+            }
+
+    public void clearEditText(){
+        et1.getText().clear();
+        et2.getText().clear();
+        et3.getText().clear();
     }
-    public void Multiply(View v){
-        EditText et1 =(EditText)findViewById(R.id.editTextNumber1);
-        EditText et2 =(EditText)findViewById(R.id.editTextNumber2);
-        EditText et4 =(EditText)findViewById(R.id.editTextNumber4);
-        EditText et3 =(EditText)findViewById(R.id.editTextNumber3);
-
-        int n1 = Integer.parseInt(et1.getText().toString());
-        int n2 = Integer.parseInt(et2.getText().toString());
-        int n4 = Integer.parseInt(et4.getText().toString());
-        int result = n1*n2*n4;
-
-        et3.setText("Multiply Value" +result);
-    }
-    public void Divide(View v){
-        EditText et1 =(EditText)findViewById(R.id.editTextNumber1);
-        EditText et2 =(EditText)findViewById(R.id.editTextNumber2);
-        EditText et4 =(EditText)findViewById(R.id.editTextNumber4);
-        EditText et3 =(EditText)findViewById(R.id.editTextNumber3);
-
-        int n1 = Integer.parseInt(et1.getText().toString());
-        int n2 = Integer.parseInt(et2.getText().toString());
-        int n4 = Integer.parseInt(et4.getText().toString());
-        int result = n1/n2/n4;
-
-        et3.setText("Divide Value" +result);
-    }
-}
+        }
